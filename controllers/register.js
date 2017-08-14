7c5f8eebf691065c77dcd204cb50448953b9da23
lulu2184 <lulu2184@gmail.com>
2017-08-15 02:11:02 +0800

    registration page

---
+++ controllers/register.js
@@ -0,0 +1,22 @@
+var sql = require('mysql');
+
+module.exports = function(dbConnection) {
+    var handler = {
+        post: function(req, res) {
+            console.log('receive login request.');
+            console.log('data: ' + JSON.stringify(req.body));
+
+            var query = sql.format('insert ignore into User (username, password, email) values(?, ?, ?)',
+                [req.body.username, req.body.password, req.body.email]);
+            console.log(query);
+            dbConnection.query(query, function(err, result){
+                if (err) throw err;
+                console.log(JSON.stringify(result));
+                if (result.affectedRows == 0) {
+                    res.json({success: false, msg: 'Username is already taken.'});
+                }
+            });
+        }
+    };
+    return handler;
+};
--- routes.js
+++ routes.js
@@ -1,8 +1,17 @@
 var routes = require('./controllers');
 
 module.exports = function(app, dbConnection) {
-    app.get('/', routes.index);
+    app.get('/', function(req, res) {
+        res.render('index');
+    });
+
+    app.get('/register', function(req, res) {
+        res.render('register');
+    });
 
     var loginHandler = require('./controllers/login')(dbConnection);
     app.post('/login', loginHandler.post);
+
+    var registerHandler = require('./controllers/register')(dbConnection);
+    app.post('/register', registerHandler.post);
 };
